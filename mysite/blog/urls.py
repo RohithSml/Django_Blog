31574914b1251ac766151871a56e706d9f454a82
RohithSml <rohith.sml94@gmail.com>
2019-09-29 22:44:03 +0530

    Adds Forms and the admin can now post content in the webpage

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
+
+
 
 def post_list(request):
     context = {}
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     context['posts'] = posts
     return render(request, 'blog/post_list.html', context)
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_new.html', {'form': form})
